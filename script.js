2c1f1ae121c9af77a76feed776803683566484b4
tonyjung78 <tjung8@jhu.edu>
2022-09-10 19:10:53 -0400

    Added logo and some js functionality:

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const aptButton = document.getElementById("apt");
 
+const noteSubmit = document.getElementById("noteSubmit");
+
 aptButton.addEventListener("click", storeData);
 aptButton.addEventListener("click", loadPatient);
+noteSubmit.addEventListener("click", addNote);
 
 function storeData() {
     localStorage.setItem("name", document.getElementById("patientName").value);
@@ -33,4 +36,22 @@ function addAppointment() {
         // cell5.innerHTML = <a href="#">x</a>;
         cell5.innerHTML = '<a href="#">x</a>';
     }
+}
+
+function addNote() {
+    console.log("hi");
+    const date = document.getElementById("noteYear").value + "/" + document.getElementById("noteMonth") + "/" +document.getElementById("noteDays");
+    const area = document.getElementById("area").value;
+    const title = document.getElementById("noteTitle").value;
+    const noteTable = document.getElementById("noteTable");
+    const noteTableLength = noteTable.row.length;
+    const row = aptTable.insertRow(noteTableLength);
+    const cell0 = row.insertCell(0)
+    const cell1 = row.insertCell(1)
+    const cell2 = row.insertCell(2)
+    const cell3 = row.insertCell(3)
+    cell0.innerHTML = tableLength;
+    cell1.innerHTML = date;
+    cell2.innerHTML = area;
+    cell3.innerHTML = title;
 }
